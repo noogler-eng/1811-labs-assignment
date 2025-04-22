487e9efcc47d09145d8ee14c4a9da7a0ecaa927c
noogler-eng <sharadpoddar10102003@gmail.com>
2025-04-23 01:14:12 +0530

    add

--- src/components/authProvider.tsx
+++ src/components/authProvider.tsx
@@ -51,7 +51,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: `https://notes.sharad.engineer/dashboard`,
         },
       }),
     signOut: () => supabase.auth.signOut(),
